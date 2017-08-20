19b1b95905ab1ad4061258f743fcb2d91f06ccc7
focus <focus@focus-desktop>
2017-08-20 15:02:05 -0400

    stray migration

---
+++ categories/migrations/0003_auto_20170820_1845.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('categories', '0002_auto_20170217_1111'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='category',
+            managers=[
+            ],
+        ),
+    ]
